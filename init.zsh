26a56b7c811494d7315eea222ef3b4490dc855dd
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-06-14 19:03:20 -0400

    init

---
+++ init.zsh
@@ -0,0 +1,15 @@
+p6df::modules::p6perl::version() { echo "0.0.1" }
+p6df::modules::p6perl::deps()    { }
+p6df::modules::p6perl::external::brew() { }
+
+p6df::modules::p6perl::init() {
+
+    p6_perl_init $P6_DFZ_DATA_PARENT/p6m7g8/p6perl
+}
+
+p6_perl_init() {
+    local dir="$1"
+
+    p6dfz::util::path_if "$dir/bin"
+    export PERL5LIB=$dir/lib/perl5
+}
