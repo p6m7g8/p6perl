d020f140cd996382c8814307c8f6a653c4eaadd6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-06-16 21:36:32 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -10,6 +10,6 @@ p6df::modules::p6perl::init() {
 p6_perl_init() {
     local dir="$1"
 
-    p6dfz::util::path_if "$dir/bin"
+    p6df::util::path_if "$dir/bin"
     export PERL5LIB=$dir/lib/perl5
 }
