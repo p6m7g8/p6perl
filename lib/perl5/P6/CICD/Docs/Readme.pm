cdc3837dc831736c122eb1d3d99d49626be22ec1
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-11-14 17:37:04 -0500

    feat(README): flush out README.md

--- lib/perl5/P6/CICD/Docs/Readme.pm
+++ lib/perl5/P6/CICD/Docs/Readme.pm
@@ -45,11 +45,52 @@ sub readme_gen() {
 
     my $module = $self->module();
 
+    print "TAG LINE\n";
+
+    print "# $module\n\n";
+
+    print "## Table of Contents\n\n";
+    print "
+### $module
+- [$module](#$module)
+  - [Badges](#badges)
+  - [Distributions](#distributions)
+  - [Summary](#summary)
+  - [Contributing](#contributing)
+  - [Code of Conduct](#code-of-conduct)
+  - [Changes](#changes)
+    - [Usage](#usage)
+  - [Author](#author)
+
+### Badges
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/p6m7g8/$module)
+[![Mergify](https://img.shields.io/endpoint.svg?url=https://gh.mergify.io/badges/p6m7g8/$module/&style=flat)](https://mergify.io)
+
+## Summary
+
+## Contributing
+
+- [How to Contribute](CONTRIBUTING.md)
+
+## Code of Conduct
+
+- [Code of Conduct](CODE_OF_CONDUCT.md)
+
+## Changes
+
+- [Change Log](CHANGELOG.md)
+
+### Usage
+
+";
+
     my $funcs = $self->funcs();
     foreach my $file ( sort keys %{ $self->funcs() } ) {
         my $section = ( split /\//, $file )[-1];
 
-        print "### $section:\n";
+        print "#### $section:\n\n";
 
         my @funcs = sort @{ $funcs->{$file} };
         foreach my $func (@funcs) {
@@ -58,6 +99,12 @@ sub readme_gen() {
         }
         print "\n";
     }
+
+    print "
+## Author
+
+Philip M . Gollucci <pgollucci\@p6m7g8.com>
+";
 
     return;
 }
